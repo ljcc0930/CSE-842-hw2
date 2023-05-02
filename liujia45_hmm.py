4482f3e0a5c4f9c880d6bcf53291f44ebb5e3fab
JC Liu <ljcc0930@qq.com>
2023-05-01 20:38:01 -0400

    Update

--- liujia45_hmm.py
+++ liujia45_hmm.py
@@ -12,7 +12,7 @@ words, word_class = zip(*data)
 # calculate transition:
 A = np.zeros((12, 12))
 tags = ['ADJ', 'ADP', 'ADV', 'CONJ', 'DET',
-          'NOUN', 'NUM', 'PRT', 'PRON', 'VERB', '.', 'X']
+        'NOUN', 'NUM', 'PRT', 'PRON', 'VERB', '.', 'X']
 tag_dict = {t: idx for idx, t in enumerate(tags)}
 for i in range(len(words) - 1):
     A[tag_dict[word_class[i]], tag_dict[word_class[i+1]]] += 1
@@ -20,7 +20,7 @@ A /= A.sum(axis=1)[:, np.newaxis]
 # print(f"A: {A}")
 print("| source \\ target | " + " | ".join(tags) + " |")
 print("---".join("|" * (len(tags) + 2)))
-for i,j in zip(A, tags):
+for i, j in zip(A, tags):
     print(f"| {j}" + "".join([" | {:.2f}".format(x) for x in i]) + " |")
 
 # calculate emission:
@@ -35,5 +35,5 @@ for i in range(12):
         B[i] /= B[i].sum()
 print("| word \\ observation | " + " | ".join(obs_dict) + " |")
 print("---".join("|" * (len(obs_dict) + 2)))
-for i,j in zip(B, tags):
+for i, j in zip(B, tags):
     print(f"| {j}" + "".join([" | {:.2f}".format(x) for x in i]) + " |")
